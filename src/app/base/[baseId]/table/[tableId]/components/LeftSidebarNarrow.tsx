4350a1bb05b450ccd6012dd1b5be2e1e94378fe4
jinohyoum <jinohyoum@gmail.com>
2025-12-27 02:02:12 +1100

    ui(LeftSidebarNarrow): home button ui 1:1

--- src/app/base/[baseId]/table/[tableId]/components/LeftSidebarNarrow.tsx
+++ src/app/base/[baseId]/table/[tableId]/components/LeftSidebarNarrow.tsx
@@ -23,11 +23,12 @@ export default function LeftSidebarNarrow() {
           <Link 
             id="appTopBarHomeButton" 
             aria-label="Back to home" 
-            className="flex flex-none relative pointer circle focus-visible-white colors-border-emphasis" 
+            className="flex flex-none relative pointer rounded-full border-transparent group" 
             href="/" 
             style={{ width: '24px', height: '24px' }}
+            aria-description="Tooltip: Back to home"
           >
-            <div className="animate flex flex-auto items-center justify-center" style={{ transform: 'scale(1)' }}>
+            <div className="transition-all duration-[85ms] ease-in flex flex-auto items-center justify-center group-hover:opacity-0" style={{ transform: 'scale(1)' }}>
               <div style={{ position: 'relative', top: '3.5px' }}>
                 <svg 
                   width="24" 
@@ -52,6 +53,16 @@ export default function LeftSidebarNarrow() {
                   </g>
                 </svg>
               </div>
+            </div>
+            {/* Back arrow overlay */}
+            <div className="transition-all duration-[85ms] ease-in absolute inset-0 opacity-0 group-hover:opacity-100 flex items-center justify-center">
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none">
+                <path 
+                  fillRule="evenodd" 
+                  d="M5.64775 2.22725C5.86742 2.44692 5.86742 2.80308 5.64775 3.02275L3.233 5.4375H10.125C10.4357 5.4375 10.6875 5.68934 10.6875 6C10.6875 6.31066 10.4357 6.5625 10.125 6.5625H3.233L5.64775 8.97725C5.86742 9.19692 5.86742 9.55308 5.64775 9.77275C5.42808 9.99242 5.07192 9.99242 4.85225 9.77275L1.47725 6.39775C1.37176 6.29226 1.3125 6.14918 1.3125 6C1.3125 5.85082 1.37176 5.70774 1.47725 5.60225L4.85225 2.22725C5.07192 2.00758 5.42808 2.00758 5.64775 2.22725Z" 
+                  fill="rgb(29, 31, 37)"
+                />
+              </svg>
             </div>
           </Link>
         </div>
